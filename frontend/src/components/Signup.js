3df6c63a17229decd674e97fb8f77bd220023866
Varun Jain <varunsj18@gmail.com>
2019-11-18 14:40:44 -0800

    Added Signup Page (unformatted), Added Main and App.js, Dummy Home Page for testing

---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,94 @@
+import React, {Component} from "react";
+import axios from "axios";
+import { Redirect } from "react-router";
+
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: ""
+      },
+      errors: {},
+      dbErrors: ""
+    };
+  }
+
+  doSubmit = () => {
+
+  };
+
+
+  render() {
+    return (
+      <div className="home">
+        <div className="login-one">Sign up for Instagram</div>
+        <br />
+        <div className="login-two">
+          Already have an account? <a href="/login">Login</a>
+        </div>
+
+        <div className="login-container">
+          <div className="container">
+            <div className="form-header">Account Sign up</div>
+            <hr />
+
+            <input
+              type="input"
+              name="firstname"
+              autoFocus
+              className="form-control"
+              placeholder="First Name"
+              onChange={this.handleChange}
+              value={this.state.firstname}
+              error={this.state.errors.firstname}
+            />
+            {this.state.errors.firstname && (
+              <div className="red">{this.state.errors.firstname} </div>
+            )}
+            <br />
+            <input
+              type="input"
+              name="lastname"
+              className="form-control"
+              placeholder="Last Name"
+              onChange={this.handleChange}
+              value={this.state.lastname}
+            />
+            <br />
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Email Address"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              placeholder="Password"
+              onChange={this.handleChange}
+              value={this.state.password}
+              error={this.state.errors.password}
+            />
+            <form onSubmit={this.handleSubmit}>
+              <button type="submit" className="login-btn">
+                Sign up
+              </button>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Signup;
