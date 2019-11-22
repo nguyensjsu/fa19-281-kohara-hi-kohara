841581a5f55d72aea87cec58fe09e904fad05345
Saket Thakare <saketthakare@gmail.com>
2019-11-22 04:55:28 -0800

    Test Frontend GKE CICD

--- frontend-docker/src/App.js
+++ frontend-docker/src/App.js
@@ -25,22 +25,7 @@ function App() {
     return (
 
         <div>
-            <nav className="navbar navbar-light" style={navbarStyles}>
-                <a className="navbar-brand" href="https://www.bookmarks.dev">
-                    <img src="/logo.png" width="30" height="30"
-                         className="d-inline-block align-top" alt=""/>
-                    &nbsp; Bookmarks - environment <b>{window.REACT_APP_ENVIRONMENT}</b>
-                </a>
-            </nav>
-            {data.map((bookmark) => (
-                <div className="card">
-                    <div className="card-body">
-                        <h5 className="card-title">{bookmark.name}</h5>
-                        <a href={bookmark.location}><h6 className="card-subtitle mb-2 text-muted">{bookmark.location}</h6></a>
-                        <p className="card-text">{bookmark.description}</p>
-                    </div>
-                </div>
-            ))}
+            <image src="https://i.imgur.com/C7jbKhd.png"></image>
         </div>
     );
 }
