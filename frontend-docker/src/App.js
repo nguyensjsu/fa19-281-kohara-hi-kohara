a09eb58c8ec7d29626eb49d40150865395b5e97e
Saket Thakare <saketthakare@gmail.com>
2019-11-22 03:43:54 -0800

    Frontend GKE deploy attempt

--- frontend-docker/src/App.js
+++ frontend-docker/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState([]);
+
+    let navbarStyles = {
+        backgroundColor: window.REACT_APP_NAVBAR_COLOR
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            const result = await axios(
+                window.REACT_APP_API_URL,
+            );
+            setData(result.data);
+        };
+        fetchData();
+    }, []);
+
+
+    return (
+
+        <div>
+            <nav className="navbar navbar-light" style={navbarStyles}>
+                <a className="navbar-brand" href="https://www.bookmarks.dev">
+                    <img src="/logo.png" width="30" height="30"
+                         className="d-inline-block align-top" alt=""/>
+                    &nbsp; Bookmarks - environment <b>{window.REACT_APP_ENVIRONMENT}</b>
+                </a>
+            </nav>
+            {data.map((bookmark) => (
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">{bookmark.name}</h5>
+                        <a href={bookmark.location}><h6 className="card-subtitle mb-2 text-muted">{bookmark.location}</h6></a>
+                        <p className="card-text">{bookmark.description}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
